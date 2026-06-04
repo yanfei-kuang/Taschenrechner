dead2264e3f2ad13c796b371fc17dfa4aa8b36a0
yanfei-kuang <yanfei.kuang.de@gmail.com>
2026-06-04 08:28:48 +0200

    Add calculator source code

---
+++ Taschenrechner.cpp
@@ -0,0 +1,61 @@
+// Taschenrechner.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    double a,b; 
+
+    char op;
+
+    char wieder;
+
+    do {      
+        cout << endl;
+
+        cout << "Bitte geben Sie die Formel ein(zum Beispiel 4 + 5)" << endl;
+
+        cin >> a >> op >> b;
+
+        switch (op) {
+        case'+':
+            cout << "Ergebnis:" << a + b << endl;
+            break;
+        case'-':
+            cout << "Ergebnis:" << a - b << endl;
+            break;
+        case'*':
+            cout << "Ergebnis:" << a * b << endl;
+            break;
+        case'/':
+            if (b != 0)
+                cout << "Ergebnis:" << a / b << endl;
+            else
+                cout << "Der Divisor kann nicht null sein!" << endl;
+            break;
+        default:
+            cout << "Nicht unterstützte Operatoren!" << endl;
+        }
+
+        cout << "Weiter rechnen?(y/n):";
+
+        cin >> wieder;
+
+    } while (wieder == 'y');
+
+    cout << "Das Verfahren war beendet" << endl;
+
+        return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
